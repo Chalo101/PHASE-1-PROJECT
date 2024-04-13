60f34c075433d999e8bd979ff122a3cc5968c795
Charles <charlesmuteithia@gmail.com>
2024-04-13 18:45:18 +0300

    adds a click event listener to the button

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the reference to the "Check Holiday" button
     const checkHolidayBtn = document.getElementById('checkHolidayBtn');
+
+    // Add click event listener to the "Check Holiday" button
+    checkHolidayBtn.addEventListener('click', function() {
+    });
 });
